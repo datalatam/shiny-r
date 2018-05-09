367d0283d5136d2eed0e073d6293a7f5b00ae2d8
FvD <FvD@users.noreply.github.com>
2018-05-09 13:27:22 -0600

    Ejemplos para la introducción

---
+++ introduccion/intro_shiny.R
@@ -0,0 +1,9 @@
+# Cuando creas un nuevo proyecto desde la interfaz de RStudio te va aparecer el
+# siguiente
+shiny::runApp("1-app.R")
+
+# renderUI
+shiny::runApp("1-renderui.R")
+
+# miapp (separar ui.R y server.R)
+shiny::runApp("miapp")
